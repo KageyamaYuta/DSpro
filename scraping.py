6af40458b158dc16365edd1cb049d3567e443878
KageyamaYuta <s2322097@stu.musashino-u.ac.jp>
2024-01-15 19:51:49 +0900

    修正

--- scraping.py
+++ scraping.py
@@ -4,7 +4,7 @@ import pandas as pd
 import sqlite3
 import os
 
-# スクレイピング部分（既存のコード）
+# スクレイピング部分
 url = 'https://tenki.jp/indexes/sleep/5/25/5040/22130/'
 response = requests.get(url)
 soup = BeautifulSoup(response.text, "html.parser")
@@ -16,20 +16,17 @@ def convert_text_to_value(text):
 
 converted_data = [convert_text_to_value(element.get_text(strip=True)) for element in data_elements]
 
-# データベースへの保存部分
+# データベース部分
 db_path = '/Users/kageyamayuu/univ./課題/DSpro/sleep_data.db'  # データベースファイルのパス
-
-# データベースに接続（なければ新規作成）
 conn = sqlite3.connect(db_path)
-
-# データベースにテーブルを作成（すでに存在する場合はスキップ）
 conn.execute('CREATE TABLE IF NOT EXISTS sleep_index (id INTEGER PRIMARY KEY, index_value INTEGER);')
-
-# データをデータベースに挿入
 cursor = conn.cursor()
 for value in converted_data:
     cursor.execute('INSERT INTO sleep_index (index_value) VALUES (?);', (value,))
-
-# 変更をコミットして接続を閉じる
 conn.commit()
 conn.close()
+
+# CSVファイルへの保存部分
+csv_path = '/Users/kageyamayuu/univ./課題/DSpro/'  # CSVファイルのパス
+data_frame = pd.DataFrame({'睡眠指数': converted_data})
+data_frame.to_csv(csv_path, index=False)
